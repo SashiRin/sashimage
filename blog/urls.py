7d309b84307009dda9eaaa7ae72c697519bc8785
Yukirin <ldy.nero@gmail.com>
2019-07-15 20:53:19 +0900

    Add urls for blog

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    BlogDeleteView,
+    BlogUpdateView,
+    BlogCreateView,
+    BlogDetailView,
+    BlogListView,
+    AboutView,
+)
+
+urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+]
--- sashimage/urls.py
+++ sashimage/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', include('blog.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
