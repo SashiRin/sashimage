a023589364cc1ca2dda2abd5683acbd0c4c9d9dc
Yukirin <ldy.nero@gmail.com>
2019-07-15 20:43:45 +0900

    Implement Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, default="")
+    description = models.CharField(max_length=200, default="")
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    thumb = models.ImageField(upload_to='thumb/')
+    image = models.ImageField(upload_to='image/')
+    body = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
