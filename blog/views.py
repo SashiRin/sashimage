03df7e4e98c09f3477c6bbd68a56ced1661ec29d
Yukirin <ldy.nero@gmail.com>
2019-07-15 20:50:02 +0900

    Implement views of blog and also add templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Post
+from .forms import PostForm
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_new.html'
+    success_url = reverse_lazy('home')
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_edit.html'
+    success_url = reverse_lazy('home')
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
